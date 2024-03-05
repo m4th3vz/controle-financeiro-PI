af72469071df4f51902d26adb25ba0f3fe93959a
Matheus Oliveira <m4th3vz@gmail.com>
2024-03-04 22:57:51 -0300

    Exclusão de despesas da lista
    Excluir itens inseridos na lista.

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.expense_list, name='expense_list'),
     path('add/', views.add_expense, name='add_expense'),
+    path('', views.expense_list, name='expense_list'),
+    path('add/', views.add_expense, name='add_expense'),
+    path('<int:expense_id>/delete/', views.delete_expense, name='delete_expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Expense
 
 def expense_list(request):
     expenses = Expense.objects.all()
@@ -15,3 +17,10 @@ def add_expense(request):
     else:
         form = ExpenseForm()
     return render(request, 'add_expense.html', {'form': form})
+
+def delete_expense(request, expense_id):
+    expense = get_object_or_404(Expense, id=expense_id)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/delete_expense.html', {'expense': expense})
