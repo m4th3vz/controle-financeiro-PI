aa5696d9aafb4ea6d377e87b77cbcc8f55b8f53c
Matheus Oliveira <m4th3vz@gmail.com>
2024-03-04 22:51:40 -0300

    Seletor de datas e tradução
    Mudança no idioma e data.

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Expense
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class ExpenseForm(forms.ModelForm):
+    date = forms.DateField(widget=DateInput(format='%d-%m-%Y'))
+
     class Meta:
         model = Expense
         fields = ['title', 'amount', 'date']
--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Expense(models.Model):
-    title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    title = models.CharField(max_length=100, verbose_name='título')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='valor')
+    date = models.DateField(verbose_name='data')
+
+    class Meta:
+        verbose_name = 'despesa'
+        verbose_name_plural = 'despesas'
 
     def __str__(self):
         return self.title
