1cfd6a094c6aa47a0a7fc8585cf88e887de6c5ff
Matheus Oliveira <m4th3vz@gmail.com>
2024-03-19 16:02:25 -0300

    MySQL banco de dados
    Alteração no banco de dados de SQLite para MySQL.

--- controle_gastos/settings.py
+++ controle_gastos/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "controle_gastos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Configuração do MySQL
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projetointegrador_db',
+        'USER': 'pi_univesp',
+        'PASSWORD': 'pi_univesp_grupo019_2211017',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
