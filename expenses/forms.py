806357a761824c82e63d7a057e9777f689250e8f
Matheus Oliveira <m4th3vz@gmail.com>
2024-08-21 18:47:13 -0300

    Adicionar despesa atualizado
    Layout de adicionar despesa alterado.

--- expenses/forms.py
+++ expenses/forms.py
@@ -2,19 +2,25 @@
 from django import forms
 from .models import Expense
 
-# Definição de um widget personalizado para o campo de data
 class DateInput(forms.DateInput):
     input_type = 'date'
 
-# Formulário para despesa
 class ExpenseForm(forms.ModelForm):
-    # Sobrescreve o widget para o campo de data
-    date = forms.DateField(label='Data',widget=DateInput(format='%d-%m-%Y'))
-    # Campo de informações adicionais sobre o título
-    title = forms.CharField(label='Título',max_length=20)
-    # Campo para a duração da despesa em meses
-    duration_months = forms.IntegerField(label='Deseja adicionar esta despesa por quantos meses?', min_value=1, initial=1, max_value=48)
-    # Campo de informações adicionais sobre o gasto
+    date = forms.DateField(
+        label='Data',
+        widget=DateInput(format='%Y-%m-%d'),
+    )
+    title = forms.CharField(
+        label='Título',
+        max_length=20,
+        initial=''
+    )
+    duration_months = forms.IntegerField(
+        label='Deseja adicionar esta despesa por quantos meses?',
+        min_value=1,
+        initial=1,
+        max_value=48
+    )
     observation = forms.CharField(
         label='Informações adicionais sobre este gasto (Opcional):',
         max_length=60,
