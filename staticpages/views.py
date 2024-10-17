1d9576cb07df2a9b809de46792012c5f88e7033d
Matheus Oliveira <m4th3vz@gmail.com>
2024-10-16 22:15:05 -0300

    API mercado financeiro
    API do mercado financeiro na página principal.

--- staticpages/views.py
+++ staticpages/views.py
@@ -1,9 +1,50 @@
 # staticpages/views.py
 from django.views.generic import TemplateView
+from django.shortcuts import render
+import urllib.request
+import json
 
-# Página de boas-vindas
+# Página de boas-vindas e API de dados da bolsa
 class WelcomeView(TemplateView):
     template_name = 'staticpages/welcome.html'
+    
+    def get(self, request, *args, **kwargs):
+        api_key = ''
+        url = f'https://api.polygon.io/v1/open-close/I:NDX/2024-04-05?apiKey={api_key}'
+
+        try:
+            # Usando urllib para fazer a requisição
+            with urllib.request.urlopen(url) as response:
+                # Verifica se a requisição foi bem-sucedida
+                if response.status != 200:
+                    context = {'error': 'Erro ao consultar a API.'}
+                    return render(request, self.template_name, context)
+
+                # Lendo e decodificando a resposta
+                data = json.loads(response.read().decode('utf-8'))
+
+            # Verifica se o status da resposta é OK
+            if data['status'] != 'OK':
+                context = {'error': 'Erro na resposta da API.'}
+                return render(request, self.template_name, context)
+
+            # Contexto com os dados da API
+            context = {
+                'from': data['from'],
+                'symbol': data['symbol'],
+                'open': data['open'],
+                'high': data['high'],
+                'low': data['low'],
+                'close': data['close'],
+                'after_hours': data['afterHours'],
+                'pre_market': data['preMarket']
+            }
+
+        except (urllib.error.URLError, json.JSONDecodeError) as e:
+            # Exibe a mensagem de erro diretamente na página
+            context = {'error': f'Ocorreu um erro: {str(e)}'}
+
+        return render(request, self.template_name, context)
 
 # Página de sobre
 class AboutView(TemplateView):
